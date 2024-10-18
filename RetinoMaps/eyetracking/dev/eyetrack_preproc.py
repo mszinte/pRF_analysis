71219624b3868a4495473657725c76a29300c769
sinaklg <sinakling@NIT-WIL-001.intlocal.univ-amu.fr>
2024-10-18 11:41:15 +0200

    eyetracking preprocessing pipeline

---
+++ RetinoMaps/eyetracking/dev/eyetrack_preproc.py
@@ -0,0 +1,178 @@
+"""
+-----------------------------------------------------------------------------------------
+eyetrack_preproc.py
+-----------------------------------------------------------------------------------------
+Goal of the script:
+- Preprocess BIDS formatted eyetracking data 
+-----------------------------------------------------------------------------------------
+Input(s):
+sys.argv[1]: subject 
+sys.argv[2]: task
+sys.argv[3]: session 
+sys.argv[4]: eye
+-----------------------------------------------------------------------------------------
+Output(s):
+Cleaned timeseries data per run 
+Tsv trial trigger timestamps 
+-----------------------------------------------------------------------------------------
+To run:
+cd /projects/prf_analysis/RetinoMaps/eyetracking/dev
+python extract_eyetraces.py sub-01 PurLoc ses-01 eye1
+-----------------------------------------------------------------------------------------
+"""
+
+import pandas as pd
+import json
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+import glob 
+import math
+import h5py
+import os
+from statistics import median
+from sklearn.preprocessing import MinMaxScaler
+import sys 
+from scipy.signal import detrend
+
+
+import pandas as pd
+import json
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+import glob 
+import os
+from sklearn.preprocessing import MinMaxScaler
+import sys
+from statistics import median
+from scipy.signal import detrend
+# path of utils folder  
+sys.path.insert(0, "/Users/sinakling/projects/pRF_analysis/analysis_code/utils") #TODO make path general
+from eyetrack_utils import *
+
+# --------------------- Load settings and inputs -------------------------------------
+
+def load_settings(settings_file):
+    with open(settings_file) as f:
+        settings = json.load(f)
+    return settings
+
+def load_inputs():
+    return sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+def ensure_save_dir(base_dir, subject):
+    save_dir = f"{base_dir}/{subject}/eyetracking"
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    return save_dir
+
+def load_events(main_dir, subject, ses, task): 
+    data_events = load_event_files(main_dir, subject, ses, task)
+    return data_events 
+
+# --------------------- Data Extraction ------------------------------------------------
+
+def extract_event_and_physio_data(main_dir, subject, task, ses, num_run, eye):
+    df_event_runs = extract_data(main_dir, subject, task, ses, num_run, eye, file_type="physioevents")
+    df_data_runs = extract_data(main_dir, subject, task, ses, num_run, eye, file_type="physio")
+    return df_event_runs, df_data_runs
+
+# --------------------- Preprocessing Methods -----------------------------------------
+
+def remove_blinks(data, method, sampling_rate):
+    if method == 'pupil_off':
+        return blinkrm_pupil_off(data, sampling_rate)
+    else:
+        print("No blink removal method specified")
+        return data
+
+def drift_correction(data, method, fixation_periods):
+    if method == "linear":
+        return detrend(data, type='linear')
+    elif method == 'median':
+        fixation_median = median([item for row in fixation_periods for item in row])
+        return np.array([elem - fixation_median for elem in data])
+    else:
+        print("No drift correction method specified")
+        return data
+
+def interpolate_nans(data):
+    return interpol_nans(data)
+
+def normalize_data(data):
+    print('- normalizing pupil data')
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    return scaler.fit_transform(data.reshape(-1, 1)).flatten()
+
+def downsample_data(data, original_rate, target_rate):
+    return downsample_to_targetrate(data, original_rate, target_rate)
+
+def apply_smoothing(data, method, settings):
+    if method == 'moving_avg':
+        return moving_average_smoothing(data, 1000, 50)
+    elif method == 'gaussian':
+        sigma = settings.get("sigma")
+        return gaussian_smoothing(data, 'x_coordinate', sigma)
+    else:
+        print(f"Unknown smoothing method: {method}")
+        return data
+
+# --------------------- Data Saving ----------------------------------------------------
+
+def save_preprocessed_data(data, file_path):
+    data.to_csv(file_path, sep='\t', index=False, compression='gzip')
+
+# --------------------- Main Preprocessing Pipeline ------------------------------------
+
+def main_preprocessing_pipeline():
+    # Load inputs and settings
+    subject, task, ses, eye = load_inputs()
+    settings = load_settings('/Users/sinakling/projects/pRF_analysis/RetinoMaps/eyetracking/dev/PurLoc_SacLoc/behavior_settings.json')  #TODO make path general
+    
+    # Prepare save directory
+    main_dir = settings.get('main_dir_mac')
+    file_dir_save = ensure_save_dir(f'{main_dir}/derivatives/pp_data', subject)
+    
+    # Load data
+    df_event_runs, df_data_runs = extract_event_and_physio_data(main_dir, subject, task, ses, settings['num_run'], eye)
+    print(df_event_runs[0].head()) 
+    print(df_data_runs[0].head())  
+    
+    # Preprocessing loop for each run
+    for run_idx, (df_event, df_data) in enumerate(zip(df_event_runs, df_data_runs)):
+        # from here per run 
+        eye_data_run, time_start_eye, time_end_eye = extract_eye_data_and_triggers(df_event, df_data,settings['first_trial_pattern'], settings['last_trial_pattern']) 
+        
+        # Apply preprocessing steps based on settings
+        eye_data_run = remove_blinks(eye_data_run, settings['blinks_remove'], settings['eyetrack_sampling'])
+        #eye_data_run = drift_correction(eye_data_run, settings['drift_corr'], extract_fixation_periods(eye_data_run, settings))
+        eye_data_run_x = interpolate_nans(eye_data_run[:,1])
+        eye_data_run_y = interpolate_nans(eye_data_run[:,2])
+        eye_data_run_p = interpolate_nans(eye_data_run[:,3])
+        eye_data_run_p = normalize_data(eye_data_run_p)
+        
+        eye_data_run = np.stack((eye_data_run[:,0],
+                                 eye_data_run_x, 
+                                 eye_data_run_y, 
+                                 eye_data_run_p), axis=1)
+
+         # Work in these 2 steps 
+         #TODO: make functions take 4d array, return 4d array
+        if settings.get('downsampling'):
+            eye_data_run   = downsample_data(eye_data_run, 1000, 100)
+            
+        if settings.get('smoothing'):
+            eye_data_run = apply_smoothing(eye_data_run, settings['smoothing'], settings)
+        
+        # Save the preprocessed data
+        tsv_file_path = f'{file_dir_save}/timeseries/{subject}_task-{task}_run_{run_idx+1}_eyedata.tsv.gz'
+        save_preprocessed_data(pd.DataFrame(eye_data_run, columns=['timestamp', 'x', 'y', 'pupil_size']), tsv_file_path)
+
+        # Save tsv triggers 
+        #TODO append to event files
+        
+        print(f"Run {run_idx+1} preprocessed and saved at {tsv_file_path}")
+
+if __name__ == "__main__":
+    main_preprocessing_pipeline()
---
+++ analysis_code/utils/eyetrack_utils.py
@@ -0,0 +1,239 @@
+def extract_data(main_dir, subject, task, ses, runs, eye, file_type):
+    '''read tsv.gsv data as pandas dataframes. Use information from json as columns'''
+    import json 
+    import pandas as pd
+    df_runs = []
+    for run in range(runs):
+        json_file_path = f'{main_dir}/{subject}/{ses}/func/{subject}_{ses}_task-{task}_run-0{run+1}_eyeData_recording-{eye}_{file_type}.json'
+        tsv_file_path = f"{main_dir}/{subject}/{ses}/func/{subject}_{ses}_task-{task}_run-0{run+1}_eyeData_recording-{eye}_{file_type}.tsv.gz"
+        
+
+        with open(json_file_path, 'r') as file:
+            json_data = json.load(file)
+
+        # Extract column names from the JSON
+        column_names = json_data['Columns']
+
+        df = pd.read_csv(
+            tsv_file_path, 
+            compression='gzip', 
+            delimiter='\t', 
+            header=None,  
+            names=column_names,  # Use the column names from JSON
+            na_values='n/a'  # Treat 'n/a' as NaN
+        )
+
+        df_runs.append(df)
+
+    return df_runs
+
+def extract_eye_data_and_triggers(df_event, df_data, onset_pattern, offset_pattern): 
+# Extract triggers
+# Initialize arrays to store results
+    import re 
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    
+
+    # Loop through the 'messages' column to extract the patterns
+
+    for index, row in df_event.iterrows():
+        message = row['message']
+        
+        if pd.isna(message):
+            continue  # Skip if NaN
+        
+        # Check for sequence 1 started
+        if re.search(onset_pattern, message):
+            time_start_eye = row['onset']  # Store by run index
+    
+        # Check for sequence 9 stopped
+        if re.search(offset_pattern, message):
+            time_end_eye = row['onset']
+
+    
+    # Filter for only timestamps between first and last trial 
+    eye_data_run = df_data[(df_data['timestamp'] >= time_start_eye) & 
+                    (df_data['timestamp'] <= time_end_eye)]
+
+
+    eye_data_run_array = eye_data_run[['timestamp', 'x_coordinate', 'y_coordinate', 'pupil_size']].to_numpy()
+
+    plt_1 = plt.figure(figsize=(15, 6))
+    plt.title("Experiment relevant timeseries")
+    plt.xlabel('x-coordinate', fontweight='bold')
+    plt.plot(eye_data_run_array[:,1])
+    plt.show()
+
+
+    return eye_data_run_array, time_start_eye, time_end_eye
+
+"""
+------------------- Preproc functions --------------------------------------
+
+"""
+
+import numpy as np 
+
+def blinkrm_pupil_off(samples, sampling_rate=1000):
+    """
+    ----------------------------------------------------------------------
+    blinkrm_pupil_off(samples, sampling_rate)
+    ----------------------------------------------------------------------
+    Goal of the function :
+    Replace blinks indicated by pupil = 0 with nan. 
+    ----------------------------------------------------------------------
+    Input(s) :
+    samples: 4D data to be adapted (t,X,Y,P)
+    sampling_rate: 1000 by default
+    ----------------------------------------------------------------------
+    Output(s) :
+    cleaned_samples
+    ----------------------------------------------------------------------
+    Function created by Sina Kling
+    ----------------------------------------------------------------------
+    """
+    import numpy as np 
+    import matplotlib.pyplot as plt
+    print(' - blink replacement with NaN')
+    addms2blink = 50  # ms added to end of blink
+    blink_duration_extension = int(sampling_rate / 1000 * addms2blink)
+    
+    # Detect blinks based on pupil size being 0
+    blink_indices = np.where(samples[:, 3] == 0)[0]
+    
+    blink_bool = np.zeros(len(samples), dtype=bool)
+    
+    for idx in blink_indices:
+        blink_bool[idx] = True
+    
+    # Adding 50 ms extension to the detected blinks
+    for idx in blink_indices:
+        start_idx = max(0, idx - blink_duration_extension)
+        end_idx = min(len(samples), idx + blink_duration_extension + 1)
+        blink_bool[start_idx:end_idx] = True
+    
+    # Add another 100 ms to the beginning and ending of each blink
+    smth_kernel = np.ones(int(sampling_rate / 1000 * 200)) / (sampling_rate / 1000 * 200)
+    extended_blink_bool = np.convolve(blink_bool, smth_kernel, mode='same') > 0
+    
+    # Replace blink points in the samples with NaN
+    cleaned_samples = samples.copy()
+    cleaned_samples[extended_blink_bool, 1:] = np.nan 
+
+    plt_2 = plt.figure(figsize=(15, 6))
+    plt.title("Blink removed timeseries")
+    plt.xlabel('x-coordinate', fontweight='bold')
+    plt.plot(cleaned_samples[:,1])
+    plt.show()
+    
+    return cleaned_samples
+
+
+
+def downsample_to_tr(original_data, eyetracking_rate):
+    """
+    ----------------------------------------------------------------------
+    downsample_to_tr(original_data)
+    ----------------------------------------------------------------------
+    Goal of the function :
+    Resample eyetracking signal to wanted resolution
+    ----------------------------------------------------------------------
+    Input(s) :
+    original_data : 1D eyetracking data to be resampled (x or y coordinates)
+    ----------------------------------------------------------------------
+    Output(s) :
+    reshaped_data : 1D resampled and reshaped data (x or y coordinates)
+    ----------------------------------------------------------------------
+    Function created by Sina Kling
+    ----------------------------------------------------------------------
+    """
+    from scipy.signal import resample
+    target_points_per_tr = 10  # 10 data points per 1.2 seconds
+    tr_duration = 1.2  # 1.2 sec
+    target_rate = target_points_per_tr / tr_duration  # 8.33 Hz
+
+    # Calculate total number of data points in target rate
+    eyetracking_in_sec = len(original_data) / eyetracking_rate  # 185 sec
+    total_target_points = int(eyetracking_in_sec * target_rate) # 1541
+
+    # Resample the data
+    downsampled_data = resample(original_data, total_target_points) # resample into amount of wanted data points
+
+    # Reshape into TRs
+    num_trs = int(eyetracking_in_sec / tr_duration) 
+
+    reshaped_data = downsampled_data[:num_trs * target_points_per_tr].reshape(num_trs, target_points_per_tr)
+
+    # Check new shape
+    print(reshaped_data.shape)
+
+    return reshaped_data
+
+
+def downsample_to_targetrate(original_data, eyetracking_rate, target_rate):
+    from scipy.signal import resample
+    # Calculate total number of data points in target rate
+    eyetracking_in_sec = len(original_data) / eyetracking_rate  
+    total_target_points = int(eyetracking_in_sec * target_rate) 
+    downsampled_data = resample(original_data, total_target_points) # resample into amount of wanted data points
+
+    return downsampled_data
+
+
+def moving_average_smoothing(dataframe, eyetracking_rate, window_duration): 
+    
+    # window duration to sec 
+    window_duration = window_duration / 1000
+    window_size = int(eyetracking_rate * window_duration)
+    
+
+    # Calculate SMA
+    dataframe['x_coordinate'] = dataframe['x_coordinate'].rolling(window=window_size).mean()
+    dataframe['y_coordinate'] = dataframe['y_coordinate'].rolling(window=window_size).mean()
+
+
+    return dataframe
+
+def gaussian_smoothing(df, column, sigma):
+    # Apply Gaussian smoothing with convolution
+    from scipy.ndimage import gaussian_filter1d
+    return gaussian_filter1d(df[column], sigma=sigma)
+
+"""
+
+------------- Other Utils ----------------------------------------------
+
+"""
+
+def interpol_nans(eyetracking_data):
+    print("- interpolating data")
+    nan_indices = np.isnan(eyetracking_data)
+
+    # Fill NaNs at the start and end with nearest valid values
+    if nan_indices[0]:  # If the first value is NaN
+        first_valid_idx = np.where(~nan_indices)[0][0]
+        eyetracking_data[:first_valid_idx] = eyetracking_data[first_valid_idx]
+        
+    if nan_indices[-1]:  # If the last value is NaN
+        last_valid_idx = np.where(~nan_indices)[0][-1]
+        eyetracking_data[last_valid_idx+1:] = eyetracking_data[last_valid_idx]
+
+    # Now interpolate remaining NaNs
+    eyetracking_no_nans = np.nan_to_num(eyetracking_data)
+    eyetracking_signal_interpolated = np.interp(np.arange(len(eyetracking_data)),
+                                               np.where(~nan_indices)[0],
+                                               eyetracking_no_nans[~nan_indices])
+    
+    return eyetracking_signal_interpolated
+
+def load_event_files(main_dir, subject, ses, task): 
+    import glob
+    data_events = sorted(glob.glob(r'{exp_dir}/{sub}/{ses}/func/{sub}_{ses}_task-{task}_*_events*.tsv'.format(exp_dir=main_dir, sub=subject, ses = ses, task = task)))
+    if subject == 'sub-01': 
+        ses = 'ses-01'
+        data_events = sorted(glob.glob(r'{exp_dir}/{sub}/{ses}/func/{sub}_{ses}_task-{task}_*_events*.tsv'.format(exp_dir=main_dir, sub=subject, ses = ses, task = task)))
+        
+    assert len(data_events) > 0, "No event files found"
+
+    return data_events
