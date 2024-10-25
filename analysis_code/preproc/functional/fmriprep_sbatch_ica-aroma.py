40ff2a7752f7cb7ee6cc81da2246ad669248447d
marco bedini marco.bedini@univ-amu.fr <mbedini@login01.cluster>
2024-10-25 15:33:30 +0200

    added the right file name in the example command

--- analysis_code/preproc/functional/fmriprep_sbatch_ica-aroma.py
+++ analysis_code/preproc/functional/fmriprep_sbatch_ica-aroma.py
@@ -38,8 +38,8 @@ python fmriprep_sbatch.py /scratch/mszinte/data MotConf sub-01 30 anat_only_n ar
 python fmriprep_sbatch.py /scratch/mszinte/data MotConf sub-01 30 anat_only_n aroma_n fmapfree_n skip_bids_val_y cifti_output_170k_y fsaverage_n 6 uriel.lascombes@etu.univ-amu.fr 327 b327
 
 With AROMA:
-python fmriprep_sbatch.py /scratch/mszinte/data RetinoMaps sub-01 30 anat_only_n aroma_y fmapfree_n skip_bids_val_y cifti_output_170k_y fsaverage_y 12 martin.szinte@etu.univ-amu.fr 327 b327
-python fmriprep_sbatch.py /scratch/mszinte/data RetinoMaps sub-03 30 anat_only_n aroma_y fmapfree_n skip_bids_val_y cifti_output_170k_y fsaverage_y 12 marco.bedini@univ-amu.fr 327 b327
+python fmriprep_sbatch_aroma.py /scratch/mszinte/data RetinoMaps sub-01 30 anat_only_n aroma_y fmapfree_n skip_bids_val_y cifti_output_170k_y fsaverage_y 12 martin.szinte@etu.univ-amu.fr 327 b327
+python fmriprep_sbatch_aroma.py /scratch/mszinte/data RetinoMaps sub-03 30 anat_only_n aroma_y fmapfree_n skip_bids_val_y cifti_output_170k_y fsaverage_y 12 marco.bedini@univ-amu.fr 327 b327
 
 -----------------------------------------------------------------------------------------
 Written by Martin Szinte (mail@martinszinte.net)
