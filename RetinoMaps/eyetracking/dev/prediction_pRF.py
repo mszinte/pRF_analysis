714051806fa106006083d9c15ce19a536fd07c23
sinaklg <sinakling@eduroam-1-10-142.univ-amu.fr>
2024-11-19 17:18:19 +0100

    final eyetracking changes

--- RetinoMaps/eyetracking/dev/prediction_pRF.py
+++ RetinoMaps/eyetracking/dev/prediction_pRF.py
@@ -46,15 +46,6 @@ def process_subject(subject, task, ses, analysis_info, main_dir):
     file_dir_save = ensure_save_dir(f'{main_dir}/derivatives/pp_data', subject)
     fig_dir_save = f'{file_dir_save}/figures'
     os.makedirs(fig_dir_save, exist_ok=True)
-   # h5_filename = f'{file_dir_save}/stats/{subject}_task-{task}_eyedata_sac_stats.h5'
-    
-   # with h5py.File(h5_filename, 'r') as h5_file:
-   #     time_start_trial = np.array(h5_file['time_start_trial'])
-   #     time_end_trial = np.array(h5_file['time_end_trial'])
-   #     time_start_seq = np.array(h5_file['time_start_seq'])
-    #    time_end_seq = np.array(h5_file['time_end_seq'])
-  #      time_start_eye = np.array(h5_file['time_start_eye'])
-  #      time_end_eye = np.array(h5_file['time_end_eye'])
     
     
     if subject == 'sub-01': 
@@ -68,9 +59,12 @@ def process_subject(subject, task, ses, analysis_info, main_dir):
     dfs_runs = [pd.read_csv(run, sep="\t") for run in data_events]
     
     all_run_durations = [np.cumsum(dfs['duration'] * 1000) for dfs in dfs_runs]
+
     precision_fraction_list = []
     precision_one_thrs_list = []
     
+    threshold = analysis_info['threshold']
+
     for run in range(5):
         #matfile = scipy.io.loadmat(data_mat[run])
         
@@ -101,8 +95,8 @@ def process_subject(subject, task, ses, analysis_info, main_dir):
             print(f'Saving {fig_fn}')
             fig.write_image(fig_fn)
 
-        #TODO make function in utils   
-        eucl_dist =  euclidean_distance(eye_data_all_runs,pred_x_intpl, pred_y_intpl, run)
+        
+        eucl_dist =  euclidean_distance_pur(eye_data_all_runs,pred_x_intpl, pred_y_intpl, run)
         
         precision_fraction = fraction_under_threshold(pred_x_intpl, eucl_dist)
         precision_fraction_list.append(precision_fraction)
@@ -110,7 +104,7 @@ def process_subject(subject, task, ses, analysis_info, main_dir):
         precision_file = f"{file_dir_save}/stats/precision_fraction_{subject}_run_0{run+1}.csv"
         np.savetxt(precision_file, precision_fraction, delimiter=",")
         
-        precision_one_thrs = fraction_under_one_threshold(pred_x_intpl,eucl_dist,1)
+        precision_one_thrs = fraction_under_one_threshold(pred_x_intpl,eucl_dist,threshold)
         precision_one_thrs_list.append(precision_one_thrs) 
 
 
@@ -180,7 +174,7 @@ def generate_final_figure(precision_data, colormap,thresholds):
     # Define layout
     layout = go.Layout(
         xaxis=dict(
-            title='Euclidean distance error in dva', range=[0, 3], zeroline=True, linecolor='black', showgrid=False, tickmode='linear', dtick=2 
+            title='Euclidean distance error in dva', range=[0, 6], zeroline=True, linecolor='black', showgrid=False, tickmode='linear', dtick=2 
         ),
         yaxis=dict(
             title=r'% ammount of data', range=[0, 1], zeroline=True, linecolor='black', showgrid=False
@@ -188,8 +182,10 @@ def generate_final_figure(precision_data, colormap,thresholds):
         plot_bgcolor='white',
         paper_bgcolor='white',
         font=dict(family="Arial", size=12, color="black"),
+        height=700,
+        width=480,  
         shapes=[dict(
-            type="line", x0=1, x1=1, y0=0, y1=1, line=dict(color="black", dash="dash")
+            type="line", x0=2, x1=2, y0=0, y1=1, line=dict(color="black", dash="dash")
         )]
     )
 
@@ -208,13 +204,18 @@ def generate_final_figure(precision_data, colormap,thresholds):
 
 
 def generate_ranking_figure(precision_data, colormap):
+    # Sort the precision data by "precision_one_thrs_mean" value
+    sorted_precision_data = dict(sorted(precision_data.items(), key=lambda item: item[1]["precision_one_thrs_mean"], reverse=True))
+    
+
     # Prepare data for the plot by creating a DataFrame
     plot_data = {
-        "Mean Precision under Threshold": [data["precision_one_thrs_mean"] for data in precision_data.values()],
-        "Category": ["Mean Precision"] * len(precision_data),  # Single category for all data points
-        "Subject": list(precision_data.keys()),  # Include subject identifiers for coloring
-        "Category Position": [1 + 0.01 * i for i in range(len(precision_data))]  # Slightly adjust positions
+        "Mean Precision under Threshold": [data["precision_one_thrs_mean"] for data in sorted_precision_data.values()],
+        "Category": ["Mean Precision"] * len(sorted_precision_data),  # Single category for all data points
+        "Subject": list(sorted_precision_data.keys()),  # Include sorted subject identifiers for coloring
+        "Category Position": [0.05 + 0.005 * i for i in range(len(sorted_precision_data))]  # Slightly adjust positions
     }
+
     df = pd.DataFrame(plot_data)
 
     # Create a strip plot with a single category
@@ -234,7 +235,7 @@ def generate_ranking_figure(precision_data, colormap):
     fig.update_layout(
         xaxis=dict(
             title='',  # No title for the x-axis
-            range=[0, 1.88],
+            range=[0, 0.3],
             showgrid=False,
             showticklabels=False  # Hide x-axis tick labels
         ),
@@ -254,7 +255,7 @@ def generate_ranking_figure(precision_data, colormap):
 
     # Save figure as PDF
     fig_path = "/Users/sinakling/disks/meso_shared/RetinoMaps/derivatives/pp_data/group/eyetracking"
-    fig_fn = f"{fig_path}/prf_threshold_3_ranking.pdf"
+    fig_fn = f"{fig_path}/prf_threshold_2_ranking.pdf"
     if not os.path.exists(fig_path):
         os.makedirs(fig_path)
     print(f'Saving {fig_fn}')
