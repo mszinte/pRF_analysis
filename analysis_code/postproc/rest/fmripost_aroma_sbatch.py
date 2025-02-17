18cad3db24f1400d6fb5cc826718409f9b2c000b
Marco BEDINI marco.bedini@univ-amu.fr <mbedini@login02.cluster>
2025-02-17 14:56:26 +0100

    fmriprep_91k is now the input folder

--- analysis_code/postproc/rest/fmripost_aroma_sbatch.py
+++ analysis_code/postproc/rest/fmripost_aroma_sbatch.py
@@ -21,7 +21,7 @@ Outputs: resting-state fMRI data denoised from motion artifacts with ICA-AROMA (
 Example:
 cd ~/projects/pRF_analysis/analysis_code/postproc/rest
 Basic command:
-python fmripost_aroma_sbatch.py /scratch/mszinte/data RetinoMaps sub-11 5 327 b327
+python fmripost_aroma_sbatch.py /scratch/mszinte/data RetinoMaps sub-03 5 327 b327
 -----------------------------------------------------------------------------------------
 Written by Marco Bedini (marco.bedini@univ-amu.fr)
 -----------------------------------------------------------------------------------------
@@ -72,7 +72,7 @@ slurm_cmd = """\
            log_dir=log_dir, cluster_name=cluster_name)
 
 # define singularity cmd
-singularity_cmd = "singularity run --cleanenv -B {main_dir}:/work_dir {simg} /work_dir/{project_dir}/derivatives/fmriprep/fmriprep_aroma/ /work_dir/{project_dir}/derivatives/fmripost_aroma/ participant \
+singularity_cmd = "singularity run --cleanenv -B {main_dir}:/work_dir {simg} /work_dir/{project_dir}/derivatives/fmriprep/fmriprep_91k /work_dir/{project_dir}/derivatives/fmripost_aroma participant \
         --participant-label {sub_num} -t rest \
         --nprocs {nb_procs} --omp-nthreads {nb_procs:.0f} \
         --mem {memory_val} \
