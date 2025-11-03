5ea62bf054ff974452ef22e4d3ef1beee9fc2447
Marco BEDINI <marco.bedini@univ-amu.fr>
2025-11-03 14:53:09 +0100

    tested winner take all workbench routines, bug fixes, and added VSS python script for the same purpose

--- RetinoMaps/rest/visualizations/cifti_mask_hollow_seed_viz_winner-take-all.sh
+++ RetinoMaps/rest/visualizations/cifti_mask_hollow_seed_viz_winner-take-all.sh
@@ -5,7 +5,7 @@
 ### WARNING: this script is not functional yet
 
 # Define base paths
-BASE_DIR="/scratch/mszinte/data/RetinoMaps/derivatives/pp_data/group/91k/rest/median_full_corr"
+BASE_DIR="/scratch/mszinte/data/RetinoMaps/derivatives/pp_data/group/91k/rest"
 SEED_DIR="/home/${USER}/projects/pRF_analysis/RetinoMaps/rest/mmp1_clusters/leaveout"
 
 # Create two output directories
@@ -16,7 +16,7 @@ mkdir -p "${BASE_DIR}/thresholded";
 for ROI in mPCS sPCS iPCS sIPS iIPS hMT+ VO LO V3AB V3 V2 V1; do
 
 # Threshold all full corr dscalar to include only non-zero vertices
-wb_command -cifti-reduce "${BASE_DIR}/group_ses-01_task-rest_space-fsLR_den-91k_desc-full_corr_${ROI}_median.dscalar.nii" \
+wb_command -cifti-reduce "${BASE_DIR}/median_full_corr/group_ses-01_task-rest_space-fsLR_den-91k_desc-full_corr_${ROI}_median.dscalar.nii" \
 COUNT_NONZERO "${BASE_DIR}/thresholded/group_ses-01_task-rest_space-fsLR_den-91k_desc-full_corr_${ROI}_median_non-zero.dscalar.nii";
  
 # Mask using the appropriate dscalar file
@@ -24,7 +24,7 @@ wb_command -cifti-math 'corr * (seed == 0) + (-0.999 * (seed > 0))' \
     "${BASE_DIR}/wta/group_ses-01_task-rest_space-fsLR_den-91k_desc-full_corr_${ROI}_median_wta.dscalar.nii" \
     -fixnan -0.999 \
     -var corr "${BASE_DIR}/thresholded/group_ses-01_task-rest_space-fsLR_den-91k_desc-full_corr_${ROI}_median_non-zero.dscalar.nii" \
-    -var seed "${SEED_DIR}/atlas-Glasser_space-fsLR_den-32k_filtered_ROIs_discarded_${ROI}.dscalar.nii" \
+    -var seed "${SEED_DIR}/atlas-Glasser_space-fsLR_den-32k_filtered_ROIs_leaveout_${ROI}.dlabel.nii" \
     -select 2 INDEXMAX;
 
 done
