fc21466e0d0fd97eddcf1ef86abd48ca0eb7d8e6
Marco BEDINI <marco.bedini@univ-amu.fr>
2025-11-12 14:22:57 +0100

    added partial correlation code to test on meso, and other scripts for the WTA viz

---
+++ RetinoMaps/rest/visualizations/cifti_separate_surfaces.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Goal: create GIFTI surfaces to import data in Python (nibabel-compatible)
+# Outputs go into the winner-take-all script
+
+# Base paths
+FISHERZ_PATH="/scratch/mszinte/data/RetinoMaps/derivatives/pp_data/group/91k/rest/hollow_seed_viz_fisher-z"
+FULLCORR_PATH="/scratch/mszinte/data/RetinoMaps/derivatives/pp_data/group/91k/rest/hollow_seed_viz_full_corr"
+
+# Output folder
+OUT_GIFTI="$STACKED_PATH/surfaces"
+mkdir -p "$OUT_GIFTI"
+
+# === Loop 1: Fisher-z files ===
+for FILE in "$FISHERZ_PATH"/group_ses-01_task-rest_space-fsLR_den-91k_desc-full_corr_fisher-z_*_hollow_seed.dscalar.nii; do
+  [ -e "$FILE" ] || continue
+  BASENAME=$(basename "$FILE" .dscalar.nii)
+  echo "🧠 Splitting Fisher-z: $BASENAME"
+
+  OUT_LH="$OUT_GIFTI/${BASENAME}_lh.func.gii"
+  OUT_RH="$OUT_GIFTI/${BASENAME}_rh.func.gii"
+
+  wb_command -cifti-separate "$FILE" COLUMN \
+    -metric CORTEX_LEFT "$OUT_LH" \
+    -metric CORTEX_RIGHT "$OUT_RH"
+done
+
+# === Loop 2: Full correlation files ===
+for FILE in "$FULLCORR_PATH"/group_ses-01_task-rest_space-fsLR_den-91k_desc-full_corr_*_hollow_seed.dscalar.nii; do
+  [ -e "$FILE" ] || continue
+  BASENAME=$(basename "$FILE" .dscalar.nii)
+  echo "🧠 Splitting Full correlation: $BASENAME"
+
+  OUT_LH="$OUT_GIFTI/${BASENAME}_lh.func.gii"
+  OUT_RH="$OUT_GIFTI/${BASENAME}_rh.func.gii"
+
+  wb_command -cifti-separate "$FILE" COLUMN \
+    -metric CORTEX_LEFT "$OUT_LH" \
+    -metric CORTEX_RIGHT "$OUT_RH"
+done
+
---
+++ RetinoMaps/rest/visualizations/mask_wta_maps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ATLAS_PATH="/media/marc_be/marc_be_vault1/RetinoMaps/derivatives/func_connectivity/Retinomaps_ROIs/atlas/macro_regions"
+
+    wb_command -metric-mask "group_winner_take_all_lh.shape.gii" \
+    "$ATLAS_PATH/atlas-Glasser_space-fsLR_den-32k_filtered_ROIs_discarded_left_hemi.shape.gii" \
+    "masked/group_winner_take_all_lh_masked.shape.gii" \
+    
+    wb_command -metric-mask "group_winner_take_all_rh.shape.gii" \
+    "$ATLAS_PATH/atlas-Glasser_space-fsLR_den-32k_filtered_ROIs_discarded_right_hemi.shape.gii" \
+    "masked/group_winner_take_all_rh_masked.shape.gii" \
