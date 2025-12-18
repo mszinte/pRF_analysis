4f1dec719debf6f81e2ea86b402dc48f13d1441b
mszinte <mszinte@invibe.nohost.me>
2025-12-18 10:18:37 +0100

    take out session of anatomical preproc

--- analysis_code/preproc/anatomical/flatten_sbatch.py
+++ analysis_code/preproc/anatomical/flatten_sbatch.py
@@ -11,7 +11,6 @@ sys.argv[2]: project name (correspond to directory)
 sys.argv[3]: subject (e.g. sub-01)
 sys.argv[4]: group (e.g. 327)
 sys.argv[5]: server project (e.g. b327)
-sys.argv[6]: session name (optional, e.g. ses-01)
 -----------------------------------------------------------------------------------------
 Output(s):
 preprocessed files
@@ -20,11 +19,11 @@ To run:
 1. cd to function
 >> cd ~/projects/pRF_analysis/analysis_code/preproc/anatomical/
 2. run python command
-python flatten_sbatch.py [main directory] [project name] [subject] [group] [server project] [session (optional)]
+python flatten_sbatch.py [main directory] [project name] [subject] [group]
+                         [server project]
 -----------------------------------------------------------------------------------------
 Example:
 python flatten_sbatch.py /scratch/mszinte/data MotConf sub-01 327 b327
-python flatten_sbatch.py /scratch/mszinte/data amblyo7T_prf sub-01 327 b327 ses-01
 -----------------------------------------------------------------------------------------
 Written by Martin Szinte (martin.szinte@gmail.com)
 Edited by Uriel Lascombes (uriel.lascombes@laposte.net)
@@ -41,13 +40,6 @@ project_dir = sys.argv[2]
 subject = sys.argv[3]
 group = sys.argv[4]
 server_project = sys.argv[5]
-session = sys.argv[6] if len(sys.argv) > 6 else None
-
-# Handle session parameter
-if session:
-    subject_name = f"{subject}_{session}"
-else:
-    subject_name = subject
 
 sub_num = subject[-2:]
 hemis = ['lh', 'rh']
@@ -76,12 +68,12 @@ for hemi in hemis:
 #SBATCH --mem={memory_val}gb
 #SBATCH --cpus-per-task={nb_procs}
 #SBATCH --time={hour_proc}:00:00
-#SBATCH -e {log_dir}/{subject_name}_{hemi}_flatten_%N_%j_%a.err
-#SBATCH -o {log_dir}/{subject_name}_{hemi}_flatten_%N_%j_%a.out
-#SBATCH -J {subject_name}_{hemi}_flatten
+#SBATCH -e {log_dir}/{subject}_{hemi}_flatten_%N_%j_%a.err
+#SBATCH -o {log_dir}/{subject}_{hemi}_flatten_%N_%j_%a.out
+#SBATCH -J {subject}_{hemi}_flatten
 export SUBJECTS_DIR='{fs_dir}'
-cd '{fs_dir}/{subject_name}/surf/'\n\n""".format(server_project=server_project, nb_procs=nb_procs, hour_proc=hour_proc, 
-                                            subject_name=subject_name, memory_val=memory_val, log_dir=log_dir, 
+cd '{fs_dir}/{subject}/surf/'\n\n""".format(server_project=server_project, nb_procs=nb_procs, hour_proc=hour_proc, 
+                                            subject=subject, memory_val=memory_val, log_dir=log_dir, 
                                             fs_dir=fs_dir, hemi=hemi)
 
     # define permission cmd
@@ -97,7 +89,7 @@ cd '{fs_dir}/{subject_name}/surf/'\n\n""".format(server_project=server_project, 
     mris_flatten {}.full.patch.3d {}.full.flat.patch.3d""".format(main_dir, project_dir, fs_dir, fs_licence, hemi, hemi)
 
     # create sh fn
-    sh_fn = "{}/{}_{}_flatten.sh".format(job_dir, subject_name, hemi)
+    sh_fn = "{}/{}_{}_flatten.sh".format(job_dir, subject, hemi)
 
     of = open(sh_fn, 'w')
     of.write("{} \n{} \n{} \n{}".format(slurm_cmd, flatten_cmd, chmod_cmd, chgrp_cmd))
