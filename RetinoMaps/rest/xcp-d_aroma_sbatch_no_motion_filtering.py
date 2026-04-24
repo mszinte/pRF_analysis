ca1917a10b96ae557930005c4a2a4d3a2606250e
Marco BEDINI <marco.bedini@univ-amu.fr>
2026-04-24 18:07:34 +0200

    parcel signals are regressed out bilaterally in partial corr now. trying to improve denoising of some runs in a few subjects

--- RetinoMaps/rest/xcp-d_aroma_sbatch_no_motion_filtering.py
+++ RetinoMaps/rest/xcp-d_aroma_sbatch_no_motion_filtering.py
@@ -81,7 +81,7 @@ slurm_cmd = """\
            log_dir=log_dir, cluster_name=cluster_name)
 
 # define singularity cmd
-singularity_cmd = "singularity run --cleanenv -B {main_dir}:/work_dir {simg} /work_dir/{project_dir}/derivatives/fmriprep/fmriprep_91k /work_dir/{project_dir}/derivatives/xcp-d participant \
+singularity_cmd = "singularity run --cleanenv -B {main_dir}:/work_dir {simg} /work_dir/{project_dir}/derivatives/fmriprep/fmriprep_91k /work_dir/{project_dir}/derivatives/xcp-d_w_atlas participant \
         --participant-label {sub_num} \
     	--mode 'linc' \
     	--bids-filter-file /work_dir/{project_dir}/derivatives/fmripost_aroma/filter_func_data.json \
