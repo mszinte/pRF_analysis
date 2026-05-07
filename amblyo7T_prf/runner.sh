170d57077776905f8ef307ba949badf0d8a5d2f1
mszinte <mszinte@invibe.nohost.me>
2026-05-07 13:57:01 +0200

    instru

--- amblyo7T_prf/runner.sh
+++ amblyo7T_prf/runner.sh
@@ -6,7 +6,7 @@ sh make_ecc_size_pcm_fig.sh /home/mszinte/projects amblyo7T_prf /scratch/mszinte
 
 # ecc comparison
 sh make_ecc_comp_tsv.sh /home/mszinte/projects amblyo7T_prf /scratch/mszinte/data 327
-sh make_ecc_comp_fig.sh [code directory] [project name] [main directory] [group]
+sh make_ecc_comp_fig.sh /home/mszinte/projects amblyo7T_prf /scratch/mszinte/data 327
 
 # copy all figures
 sh copy_subject_figs_to_group.sh /scratch/mszinte/data amblyo7T_prf
