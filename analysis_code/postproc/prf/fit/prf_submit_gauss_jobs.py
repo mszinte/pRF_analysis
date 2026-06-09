5b50af3f6bf03b124d00071ef25716b407ed1c65
sinaklg <sina.kling@outlook.de>
2026-06-09 15:38:12 +0200

    prf output folder and analysis_name, restructuring of settings (general vs specific), removing old codes from SacLoc project

--- analysis_code/postproc/prf/fit/prf_submit_gauss_jobs.py
+++ analysis_code/postproc/prf/fit/prf_submit_gauss_jobs.py
@@ -9,8 +9,9 @@ Input(s):
 sys.argv[1]: main project directory
 sys.argv[2]: project name (correspond to directory)
 sys.argv[3]: subject name (e.g. sub-01)
-sys.argv[4]: group (e.g. 327)
-sys.argv[5]: server project (e.g. b327)
+sys.argv[4]: analysis task name (ex. prf)
+sys.argv[5]: group (e.g. 327)
+sys.argv[6]: server project (e.g. b327)
 -----------------------------------------------------------------------------------------
 Output(s):
 .sh file to execute in server
@@ -19,21 +20,18 @@ To run:
 1. cd to function
 >> cd ~/projects/pRF_analysis/analysis_code/postproc/prf/fit
 2. run python command
-python prf_submit_gauss_jobs.py [main directory] [project name] [subject]
+python prf_submit_gauss_jobs.py [main directory] [project name] [subject] [analysis name]
                                 [group] [server project]
 -----------------------------------------------------------------------------------------
 Exemple:
 cd ~/projects/pRF_analysis/analysis_code/postproc/prf/fit
-python prf_submit_gauss_jobs.py /scratch/mszinte/data RetinoMaps sub-01 327 b327
-python prf_submit_gauss_jobs.py /scratch/mszinte/data amblyo7T_prf sub-01 327 b327
+python prf_submit_gauss_jobs.py /scratch/mszinte/data RetinoMaps sub-01 prf 327 b327
+python prf_submit_gauss_jobs.py /scratch/mszinte/data amblyo7T_prf sub-01 prf 327 b327
 -----------------------------------------------------------------------------------------
 Written by Martin Szinte (martin.szinte@gmail.com)
 and Uriel Lascombes (uriel.lascombes@laposte.net)
 -----------------------------------------------------------------------------------------
 """
-
-#TODO: find a a way to find the prf fit for the right format and extension & load them for the subject 
-
 # Stop warnings
 import warnings
 warnings.filterwarnings("ignore")
@@ -56,8 +54,9 @@ from settings_utils import load_settings
 main_dir = sys.argv[1]
 project_dir = sys.argv[2]
 subject = sys.argv[3]
-group = sys.argv[4]
-server_project = sys.argv[5]
+analysis_name = sys.argv[4]
+group = sys.argv[5]
+server_project = sys.argv[6]
 
 memory_val = 30
 hour_proc = 6
@@ -65,17 +64,19 @@ nb_procs = 8
 
 # Load settings
 base_dir = os.path.abspath(os.path.join(os.getcwd(), "../../../../"))
-settings_path = os.path.join(base_dir, project_dir, "settings.yml")
-prf_settings_path = os.path.join(base_dir, project_dir, "prf-analysis.yml")
-settings = load_settings([settings_path, prf_settings_path])
+general_settings_path = os.path.join(base_dir, project_dir, "settings.yml")
+analysis_settings_path = os.path.join(base_dir, project_dir, f"{analysis_name}-analysis.yml")
+settings = load_settings([general_settings_path, analysis_settings_path])
 analysis_info = settings[0]
 
 cluster_name  = analysis_info['cluster_name']
-prf_task_names = analysis_info['prf_task_names']
+task_names = analysis_info['analysis_task_names']
 preproc_prep = analysis_info['preproc_prep']
 filtering = analysis_info['filtering']
 normalization = analysis_info['normalization']
-avg_methods = analysis_info['avg_methods']
+avg_methods = analysis_info['avg_methods'] # selects input data 
+output_folder = analysis_info["output_folder"]
+dm_name = analysis_info["dm_name"]
 
 # Set pycortex db and colormaps
 cortex_dir = "{}/{}/derivatives/pp_data/cortex".format(main_dir, project_dir)
@@ -94,12 +95,12 @@ for avg_method in avg_methods:
     if "loo" in avg_method:
         continue  # Skip if it contains "loo"
     
-    for prf_task_name in prf_task_names:
+    for task_name in task_names:
         
         dct_avg_gii_fns = "{}/{}/fsnative/func/{}_{}_{}_{}/*_task-{}*{}*.func.gii".format(
-            pp_dir, subject, preproc_prep, filtering, normalization, avg_method, prf_task_name, avg_method)
+            pp_dir, subject, preproc_prep, filtering, normalization, avg_method, task_name, avg_method)
         dct_avg_nii_fns = "{}/{}/170k/func/{}_{}_{}_{}/*_task-{}*{}*.dtseries.nii".format(
-            pp_dir, subject, preproc_prep, filtering, normalization, avg_method, prf_task_name, avg_method)
+            pp_dir, subject, preproc_prep, filtering, normalization, avg_method, task_name, avg_method)
 
         # Accumulate the results
         pp_fns.extend(glob.glob(dct_avg_gii_fns))
@@ -109,19 +110,19 @@ for avg_method in avg_methods:
 for fit_num, pp_fn in enumerate(pp_fns):
 
     if pp_fn.endswith('.nii'):
-        prf_dir = "{}/{}/170k/prf".format(pp_dir, subject)
+        prf_dir = "{}/{}/170k/{}".format(pp_dir, subject, output_folder)
         os.makedirs(prf_dir, exist_ok=True)
-        prf_jobs_dir = "{}/{}/170k/prf/jobs".format(pp_dir, subject)
+        prf_jobs_dir = "{}/{}/170k/{}/jobs".format(pp_dir, subject, output_folder)
         os.makedirs(prf_jobs_dir, exist_ok=True)
-        prf_logs_dir = "{}/{}/170k/prf/log_outputs".format(pp_dir, subject)
+        prf_logs_dir = "{}/{}/170k/{}/log_outputs".format(pp_dir, subject, output_folder)
         os.makedirs(prf_logs_dir, exist_ok=True)
 
     elif pp_fn.endswith('.gii'):
-        prf_dir = "{}/{}/fsnative/prf".format(pp_dir, subject)
+        prf_dir = "{}/{}/fsnative/{}".format(pp_dir, subject, output_folder)
         os.makedirs(prf_dir, exist_ok=True)
-        prf_jobs_dir = "{}/{}/fsnative/prf/jobs".format(pp_dir, subject)
+        prf_jobs_dir = "{}/{}/fsnative/{}/jobs".format(pp_dir, subject, output_folder)
         os.makedirs(prf_jobs_dir, exist_ok=True)
-        prf_logs_dir = "{}/{}/fsnative/prf/log_outputs".format(pp_dir, subject)
+        prf_logs_dir = "{}/{}/fsnative/{}/log_outputs".format(pp_dir, subject, output_folder)
         os.makedirs(prf_logs_dir, exist_ok=True)
     
     slurm_cmd = """\
@@ -132,19 +133,19 @@ for fit_num, pp_fn in enumerate(pp_fns):
 #SBATCH --mem={memory_val}gb
 #SBATCH --cpus-per-task={nb_procs}
 #SBATCH --time={hour_proc}:00:00
-#SBATCH -e {log_dir}/{subject}_gauss_%N_%j_%a.err
-#SBATCH -o {log_dir}/{subject}_gauss_%N_%j_%a.out
-#SBATCH -J {subject}_gaussfit
+#SBATCH -e {log_dir}/{subject}_{analysis_name}-gauss-{dm_name}_%N_%j_%a.err
+#SBATCH -o {log_dir}/{subject}_{analysis_name}-gauss-{dm_name}_%N_%j_%a.out
+#SBATCH -J {subject}_{analysis_name}-gauss-{dm_name}_fit
 """.format(server_project=server_project, cluster_name=cluster_name,
            nb_procs=nb_procs, hour_proc=hour_proc, 
-           subject=subject, memory_val=memory_val, log_dir=prf_logs_dir)
+           subject=subject, memory_val=memory_val, log_dir=prf_logs_dir, analysis_name=analysis_name, dm_name=dm_name)
 
     # define fit cmd
-    fit_cmd = "python prf_gaussfit.py {} {} {} {} {}".format(
-        main_dir, project_dir, subject, pp_fn, nb_procs)
+    fit_cmd = "python prf_gaussfit.py {} {} {} {} {} {}".format(
+        main_dir, project_dir, subject, pp_fn, analysis_name nb_procs)
     
     # create sh
-    sh_fn = "{}/jobs/{}_prf_gaussfit-{}.sh".format(prf_dir, subject, fit_num)
+    sh_fn = "{}/jobs/{}_{}-gauss-{}_fit-{}.sh".format(prf_dir, subject, analysis_name, dm_name, fit_num)
     of = open(sh_fn, 'w')
     of.write("{} \n{} \n{} \n{}".format(slurm_cmd, fit_cmd, 
                                         chmod_cmd, chgrp_cmd))
